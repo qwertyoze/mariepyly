f81c75cd16eb6ce89983a99abb9004836b8909af
qwertyoze <roangel474@gmail.com>
2024-09-29 16:37:05 +0300

    final

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://qwertyoze.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/test-docusaurus-docs/',
+  baseUrl: '/mariepyly/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
