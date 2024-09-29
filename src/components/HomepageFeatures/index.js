d6285a68a4b27ad01e030df95ba9ffaf8a3506c5
qwertyoze <roangel474@gmail.com>
2024-09-29 17:40:35 +0300

    text and photos

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,32 +4,29 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'reason 1',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+       I am pretty
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'reason 2',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        I am smart
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'reason 3',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Why not?
       </>
     ),
   },
